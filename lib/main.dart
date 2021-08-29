188844d52123dcf581c3c4123d1b0a5192f0dc14
Ayushman <ayushmansengar1234@gmail.com>
2021-08-29 19:53:02 +0530

    initial commit'

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+import 'package:weatherapp/pages/home.dart';
+import 'package:weatherapp/pages/loading.dart';
+import 'package:weatherapp/pages/location.dart';
+
+
+
+
+void main() {
+  runApp(MaterialApp(
+
+    initialRoute: '/home',
+
+    routes: {
+      '/': (context)=> Loading(),
+      '/home': (context)=> Home(),
+      '/location': (context)=>Location(),
+    },
+  ));
+}
+
+
+
+
+
+
+
---
+++ lib/pages/location.dart
@@ -0,0 +1,16 @@
+
+import 'package:flutter/cupertino.dart';
+
+class Location extends StatefulWidget {
+  const Location({Key? key}) : super(key: key);
+
+  @override
+  _LocationState createState() => _LocationState();
+}
+
+class _LocationState extends State<Location> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
